062cd4618dc9a98a16f56bf047e091ef6d890ef1
Stephen Thurman <thurman024@gmail.com>
2018-02-02 02:10:24 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -36,7 +36,7 @@ class MusicLibraryController
 
   def list_songs
     Song.all.sort_by!(&:name)
-    Song.all.each_with_index {|s, i| puts "#{i}. #{s.artist.name} - #{s.song.name} - s.genre.name"}
+    Song.all.each_with_index {|s, i| puts "#{i}. #{s.artist.name} - #{s.name} - s.genre.name"}
   end
 
   def list_artists
