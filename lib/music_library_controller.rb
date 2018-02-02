a01396384bab803834da2f3d78262bd34b2f0294
Stephen Thurman <thurman024@gmail.com>
2018-02-02 02:04:21 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -17,7 +17,44 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts 'What would you like to do?'
       input = gets.chomp
-
+      case input.downcase
+      when 'list songs'
+        list_songs
+      when 'list artists'
+      list_artists
+      when 'list genres'
+        list_genres
+      when 'list artist'
+        list_songs_by_artist
+      when 'list genre'
+        list_songs_by_genre
+      when 'play song'
+        play_song
+      end
     end
+  end
+
+  def list_songs
+
+  end
+
+  def list_artists
+
+  end
+
+  def list_genres
+
+  end
+
+  def list_songs_by_artist
+
+  end
+
+  def list_songs_by_genre
+
+  end
+
+  def play_song
+
   end
 end
